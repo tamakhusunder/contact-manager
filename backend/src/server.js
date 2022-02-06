bb49400665789c33c7178bcf352b2299da8adf1b
tamakhusunder <tamakhusunder@gmail.com>
2022-02-06 18:27:48 +0545

    backend - models - user model's unwanted commit removed

--- backend/src/server.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/my_database',{useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log("connection successful.."))
-.catch((err) => console.log("err",err));
