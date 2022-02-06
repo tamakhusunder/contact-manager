ed0a88ba027b3f1ee09e80e9a860f1fef2d73b91
tamakhusunder <tamakhusunder@gmail.com>
2022-02-06 12:33:06 +0545

    backend - db(mongoose) added in index

--- backend/src/env.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-/**
- * Initialize environment variables.
- */
- PORT = '7000'
-
-dotenv.config();
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,22 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const router = express.Router();
+const mongoose = require('mongoose');
 
+// const router = express.Router();
+const userRouter = require('./route/user');
+const contactRouter = require('./route/contact');
 const APP_PORT = process.env.PORT || '5000';
+
+mongoose.connect('mongodb://localhost:27017/my_database',{useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log("database connection successful.."))
+  .catch((err) => console.log("err",err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-
-router.get('/', function (req, res) {
-    res.send('hello world')
-  });
-
-router.post('/signup', function (req, res) {
-    res.send('signin')
-  });
-
-router.post('/signin', function (req, res) {
-    res.send('login')
-  });
-
-
-
-
-router.get('/contacts', function (req, res) {
-    res.send('get')
-  });
-
-router.post('/contacts', function (req, res) {
-    res.send('post')
-  });
-router.put('/contacts/:id', function (req, res) {
-    res.send('put')
-  });
-router.delete('/contacts/:id', function (req, res) {
-    res.send('del')
-  });
-
-
-app.use("/", router);
+app.use(userRouter);
+app.use(contactRouter);
 
 app.listen(APP_PORT, () =>{
     console.log(`server running at Port : ${APP_PORT}`);
