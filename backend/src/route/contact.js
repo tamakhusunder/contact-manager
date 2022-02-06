8d21e5ad23f864b34935bd0032af845be1e8c0ec
tamakhusunder <tamakhusunder@gmail.com>
2022-02-06 18:37:03 +0545

    backend-route - CRUD operation added in contact route

--- backend/src/route/contact.js
+++ backend/src/route/contact.js
@@ -1,18 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const contactModel = require('../models/contact');
 
-router.get('/contacts', function (req, res) {
-    res.send('get')
-  });
+router.get('/contacts', (req, res) => {
+  contactModel
+    .find({})
+    .exec()
+    .then((contact) =>{
+      res.status(200).json(contact);
+    })
+    .catch((err) => {
+      res.status(500).json({error:err});
+    });
+});
 
-router.post('/contacts', function (req, res) {
-    res.send('post')
+router.post('/contacts', (req, res) => {
+  const contact = new contactModel({
+    name : req.body.name,
+    phone : req.body.phone,
+    image : req.body.image,
   });
-router.put('/contacts/:id', function (req, res) {
-    res.send('put')
+  contact
+    .save()
+    .then((result) => {
+      res.status(200).json({msg : "Contact added"});
+    })
+    .catch((err) => {
+      res.status(500).json({errror: err});
+    });
+});
+
+router.put('/contacts/:id', (req, res) => {
+  const {id} = req.params;
+  const updateValue = {
+    name : req.body.name,
+    phone : req.body.phone,
+    image : req.body.image,
+  };
+  contactModel.findByIdAndUpdate(id,updateValue, (err, docs) =>{
+    if(err) {
+      res.status(500).json(err);
+    }
+    else{
+      res.status(200).json({msg:"Data updated succesfully"});
+    }
   });
-router.delete('/contacts/:id', function (req, res) {
-    res.send('del')
+});
+
+
+router.delete('/contacts/:id',  (req, res) => {
+  const {id} = req.params;
+  contactModel.findByIdAndDelete(id, (err,docs) => {
+    if(err) {
+      res.status(500).json(err);
+    }
+    else{
+      res.status(200).json({msg:`${docs.name} data deleted`});
+    }
   });
+});
 
 module.exports = router;
