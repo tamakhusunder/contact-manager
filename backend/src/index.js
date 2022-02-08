11b5a522debb1c6281842c52b31b9456f4e2f895
tamakhusunder <tamakhusunder@gmail.com>
2022-02-08 22:36:41 +0545

    backend-index -CORS package added

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,9 @@ const contactRouter = require('./route/contact');
 const APP_PORT = process.env.PORT || '5000';
 require("./db/connection");
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
