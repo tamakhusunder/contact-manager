4053eb6f7e5ecce53771f6b3ca3e8206a752e100
tamakhusunder <tamakhusunder@gmail.com>
2022-04-09 22:07:50 +0545

    backend-index - static path for image added

--- changed-contact_manager/backend/src/index.js
+++ changed-contact_manager/backend/src/index.js
@@ -10,8 +10,10 @@ require("./db/connection");
 const cors = require("cors");
 app.use(cors());
 
-// app.use(express.static(__dirname + '/public'));
-// app.use('/uploads', express.static('uploads'));
+// app.use(express.static(__dirname));
+// app.use(express.static(__dirname + '/uploads/'));
+app.use(express.static(path.join(__dirname, "../")));
+console.log(__dirname)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
